554e7c1621265375fef6834b8f96b9545f891e74
ThomasMarques <marquesthom@gmail.com>
2025-11-27 13:45:12 +0100

    Force install deps for Azure

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-PWD=$(pwd)
-echo "Current working directory: $PWD"
-ls -al
+pip install -Ur requirements.txt
 pip list
 uvicorn --workers 4 good_wine_api.main:app --host 0.0.0.0 --port 8000
