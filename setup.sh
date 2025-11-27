2e0d215da8b03f1e9700d1ede653482e1e23513f
ThomasMarques <marquesthom@gmail.com>
2025-11-27 13:15:50 +0100

    Use requirements.txt

--- setup.sh
+++ setup.sh
@@ -2,8 +2,5 @@
 PWD=$(pwd)
 echo "Current working directory: $PWD"
 ls -al
-ls -al venv/bin/
-export PATH="$PWD/venv/bin:$PATH"
-source venv/bin/activate
-ls -al $PWD/venv/bin/uvicorn
-$PWD/venv/bin/uvicorn --workers 4 good_wine_api.main:app --host 0.0.0.0 --port 8000
+pip list
+uvicorn --workers 4 good_wine_api.main:app --host 0.0.0.0 --port 8000
