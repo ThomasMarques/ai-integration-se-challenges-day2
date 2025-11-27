9b38559def6c551e77a0747ceae732ad66a8933d
ThomasMarques <marquesthom@gmail.com>
2025-11-27 12:45:30 +0100

    Debug Azure venv

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
-venv/bin/uvicorn --workers 4 good_wine_api.main:app
+#!/bin/bash
+PWD=$(pwd)
+echo "Current working directory: $PWD"
+ls -al
+ls -al venv/bin/
+export PATH="$PWD/venv/bin:$PATH"
+source venv/bin/activate
+ls -al $PWD/venv/bin/uvicorn
+$PWD/venv/bin/uvicorn --workers 4 good_wine_api.main:app --host 0.0.0.0 --port 8000
